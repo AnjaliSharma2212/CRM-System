a742fd33dff92956308693d7393633c73ec2ec15
Anjali Sharma <anjalivce19@gmail.com>
2025-11-14 15:54:18 +0530

    url

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ const server = http.createServer(app);
 // SOCKET.IO SERVER
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://crmme-system.netlify.app/",
     methods: ["GET", "POST"]
   }
 });
